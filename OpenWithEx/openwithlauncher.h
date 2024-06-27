ff9f1fd303348ebd7c81d0858be58d7b49ed9870
aubymori <aubyomori@gmail.com>
2024-06-26 22:22:43 -0500

    If I'm correct, this isn't needed

--- OpenWithEx/openwithlauncher.h
+++ OpenWithEx/openwithlauncher.h
@@ -11,12 +11,6 @@ enum IMMERSIVE_OPENWITH_FLAGS
 class IOpenWithLauncher : public IUnknown
 {
 public:
-	/* Begin IUnknown implementation */
-	virtual HRESULT QueryInterface(REFIID riid, void **ppvObject) = 0;
-	virtual ULONG AddRef() = 0;
-	virtual ULONG Release() = 0;
-	/* End IUnknown implementation */
-
 	virtual HRESULT Launch(HWND hWndParent, LPCWSTR lpszPath, IMMERSIVE_OPENWITH_FLAGS flags) = 0;
 };
 
