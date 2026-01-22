60ae82bfee89edd8a9471d7dd27db1babc4b10a6
aubymori <aubyomori@gmail.com>
2026-01-21 23:01:15 -0600

    Lots of work

--- src/openwithex.cpp
+++ src/openwithex.cpp
@@ -1,4 +1,5 @@
 #include "openwithex_priv.h"
+#include <stdio.h>
 
 int WINAPI wWinMain(
 	HINSTANCE hInstance,
@@ -7,8 +8,21 @@ int WINAPI wWinMain(
 	int       nShowCmd
 )
 {
-	int nArgs;
-	LPWSTR *ppszArgs = CommandLineToArgvW(lpCmdLine, &nArgs);
+	// CommandLineToArgvW will set the first argument to the program name if the
+	// string is totally empty. This behavior is stinky so if our cmd line is
+	// empty we don't even bother.
+	int nArgs = 0;
+	LPWSTR *ppszArgs = nullptr;
+	if (lpCmdLine[0] != L'\0')
+	{
+		ppszArgs = CommandLineToArgvW(lpCmdLine, &nArgs);
+		RETURN_LAST_ERROR_IF_NULL(ppszArgs);
+	}
+
+	for (int i = 0; i < nArgs; i++)
+	{
+		MessageBoxW(NULL, ppszArgs[i], L"hi", MB_ICONINFORMATION);
+	}
 
 	return 0;
 }
--- src/openwithex_priv.h
+++ src/openwithex_priv.h
@@ -1,4 +1,5 @@
 #pragma once
 #include "openwithex.h"
-#include "wil/result_macros.h"
+#include <wil/result_macros.h>
+#include <wil/resource.h>
 #include <shlwapi.h>
